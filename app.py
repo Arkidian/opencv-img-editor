0a6fbece95874fe5424ae165037a98d11004f8c0
Rai <2472319246@qq.com>
2022-07-17 20:55:08 +0800

    update functions

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import cv2
 from flask import Flask, request
 from flask_uploads import UploadSet, configure_uploads, IMAGES,\
  patch_request_class
-from output import out_path
+from output import out_path , web_path
 from program import task
 
 app = Flask(__name__)
@@ -18,7 +18,7 @@ patch_request_class(app)  # 文件大小限制，默认为16MB
 photo_name = 'Tsunami_by_hokusai_19th_century.jpg'
 photo_url =os.getcwd() + '\\static\\uploads\\' + 'Tsunami_by_hokusai_19th_century.jpg'
 # 现在的图片的版本
-photo_version = 0
+app.config['cnt'] = 0
 
 html = '''
     <!DOCTYPE html>
@@ -30,10 +30,15 @@ html = '''
     </form>
     '''
 
+pure_html = '''
+	<!DOCTYPE html>
+	<title>Processed Image</title>
+	'''
 
 # 主页面
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    cnt = 0
     del_all()
     if request.method == 'POST' and 'photo' in request.files:
         filename = photos.save(request.files['photo'])
@@ -41,7 +46,9 @@ def upload_file():
         app.photo_name = filename
         file_url = photos.url(filename)
         app.photo_url = file_url
-        print(app.photo_name)
+        img = cv2.imread(photo_url)  # 根据路径读取一张图片
+        cv2.imwrite(out_path(0), img)
+        print(file_url)
         return html + '<br><img src=' + file_url + '>'
     return html
 
@@ -64,34 +71,25 @@ def del_file(path):
         os.remove(path)
 
 
+# 通过commmand_id来选择处理的方式，commmand_id具体内容再program.py里面找
 @app.route('/deal/<command_id>')
 def deal(command_id):
-    cnt = 0
     img = cv2.imread(photo_url)  # 根据路径读取一张图片
-    cv2.imwrite(out_path(cnt), img)
 
     cmd = command_id
 
-    if (cmd == 'q' or cmd == 'quit'):
-        # 退出进程
-        for id in range(0, cnt + 1):
-            if os.path.exists(out_path(id)):
-                os.remove(out_path(id))
-            print('quit success')
-            break
-
-    elif (cmd == 'b' or cmd == 'back'):
+    if (cmd == 'b' or cmd == 'back'):
         # 撤销操作
-        if (cnt > 0):
-            if os.path.exists(out_path(cnt)):
-                os.remove(out_path(cnt))
-            cnt = cnt - 1
+        if (app.config['cnt'] > 0):
+            if os.path.exists(out_path(app.config['cnt'])):
+                os.remove(out_path(app.config['cnt']))
+            app.config['cnt'] = app.config['cnt'] - 1
             print('back success')
 
     elif (cmd == 's' or cmd == 'save'):
         # 保存操作
-        cnt_max = cnt
-        img = cv2.imread(out_path(cnt))
+        cnt_max = app.config['cnt']
+        img = cv2.imread(out_path(app.config['cnt']))
         cv2.imwrite(os.getcwd() + '\\static', img)
         cnt = 0
         cv2.imwrite(out_path(cnt), img)
@@ -102,10 +100,13 @@ def deal(command_id):
 
     else:
         # 图像处理操作
-        success = task(cmd, cnt)
+        success = task(cmd, app.config['cnt'])
         if (success == 0):
-            cnt = cnt + 1
-    return "修改成功"
+            app.config['cnt'] = app.config['cnt'] + 1
+        str_cnt = str(app.config['cnt'])
+        print(type(str_cnt))
+        print(app.config['cnt'])
+    return pure_html + web_path(app.config['cnt'])
 
 if __name__ == '__main__':
     app.run()
--- output.py
+++ output.py
@@ -1,6 +1,16 @@
 import os
 
-
+#文件存储path
 def out_path(cnt):
     file_name = os.getcwd() + '\\static\\uploads\\' + str(int(cnt)) + '.jpg'
     return file_name
+
+# 图片的映射：相当于图片外链
+def direct_path(cnt):
+    dp = 'http://127.0.0.1:5000/_uploads/photos/' + str(int(cnt)) + '.jpg'
+    return dp
+
+# 图片嵌入式代码：可以直接嵌入于html中
+def web_path(cnt):
+    website_path = '<br><img src=http://127.0.0.1:5000/_uploads/photos/' + str(int(cnt)) +'.jpg>'
+    return website_path
