d8f911082516b466b84111c3ca2ee3eac132c667
Rai <2472319246@qq.com>
2022-07-22 22:50:29 +0800

    fix bugs

--- output.py
+++ output.py
@@ -10,6 +10,10 @@ def direct_path(cnt):
     dp = 'http://127.0.0.1:5000/_uploads/photos/' + str(int(cnt)) + '.jpg'
     return dp
 
+def file_name(cnt):
+    name = str(int(cnt)) + '.jpg'
+    return name
+
 # 图片嵌入式代码：可以直接嵌入于html中
 def web_path(cnt):
     website_path = '<br><img src=http://127.0.0.1:5000/_uploads/photos/' + str(int(cnt)) +'.jpg>'
